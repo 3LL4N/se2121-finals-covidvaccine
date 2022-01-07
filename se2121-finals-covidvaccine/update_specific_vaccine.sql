75e04a7d4d95d318eb15de9af40c2e18f0ca1a43
unknown <dierongamer04@gmail.com>
2022-01-07 09:47:45 +0800

    Updated last item of no.3 to a working state

--- se2121-finals-covidvaccine/update_specific_vaccine.sql
+++ se2121-finals-covidvaccine/update_specific_vaccine.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE update_vaccine(
     vaccine_name varchar(255),
     vaccine_location varchar(255),
-    vaccine_date date,
+    vaccine_date varchar(255),
     update_vaccination int
 )
 LANGUAGE plpgsql
@@ -13,7 +13,7 @@ BEGIN
    SET vbm_total_vaccinations = update_vaccination 
    WHERE vbm_vaccine = vaccine_name
    AND vbm_location = vaccine_location
-   AND vbm_date = vaccine_date;
+   AND vbm_date::varchar(255) = vaccine_date;
 
 END;
 $$
