3cfbee1a5ce4696f8c451a7897b784aecee348f8
unknown <dierongamer04@gmail.com>
2022-01-07 23:20:45 +0800

    Finished 6 and added codes for 5b

--- se2121-finals-covidvaccine/(5b)vaccinations_per_month_per_vaccine.sql
+++ se2121-finals-covidvaccine/(5b)vaccinations_per_month_per_vaccine.sql
@@ -1,12 +1,24 @@
-CREATE FUNCTION (
+CREATE FUNCTION vaccine_per_month(
     location_name varchar(255),
     location_date varchar(255)
 )
-RETURNS varchar(255)
-LANGUAGE plpgsql
+    RETURNS TABLE(
+        vacc_date varchar(255),
+        vacc_brand varchar(255),
+        vacc_total bigint
+    )
+    LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    
+
+    RETURN QUERY
+
+    SELECT vbm_date, vbm_vaccine, SUM(vbm_total_vaccinations) AS brand_total
+    FROM vaccinations_by_manufacturer
+    WHERE vbm_location = location_name 
+        AND date_trunc('year',  TO_TIMESTAMP(vbm_date, 'YYYY-MM-DD HH:MI:SS'))::varchar(255)= location_date
+        AND date_trunc('month',  TO_TIMESTAMP(vbm_date, 'YYYY-MM-DD HH:MI:SS'))::varchar(255) = location_date
+    GROUP BY vbm_date, vbm_vaccine;
 END;
 $$
--- se2121-finals-covidvaccine/(6)daily_avg_vaccinations.sql
+++ se2121-finals-covidvaccine/(6)daily_avg_vaccinations.sql
@@ -1,4 +1,10 @@
-CREATE VIEW VaccineAverage 
-AS SELECT AVG(vbl_id) 
-FROM vaccinations_by_location 
-GROUP BY vbl_iso_code, vbl_vaccines;
+CREATE VIEW vaccine_average AS 
+SELECT loc_ios_code, vbm_vaccine, AVG(vbm_total_vaccinations) 
+FROM vaccinations_by_manufacturer
+INNER JOIN locations
+ON vaccinations_by_manufacturer.vbm_location = locations.loc_name
+GROUP BY loc_ios_code, vbm_location, vbm_vaccine
+ORDER BY vbm_location;
+
+
+
